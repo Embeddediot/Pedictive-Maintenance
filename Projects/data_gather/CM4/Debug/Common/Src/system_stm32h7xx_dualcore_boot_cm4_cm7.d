81d18f57d94056ed4e81ac6111016b81cf448044
embeddediot <s172533@student.dtu.dk>
2024-06-05 10:03:00 +0200

    Added stts22 example code

--- Projects/data_gather/CM4/Debug/Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.d
+++ Projects/data_gather/CM4/Debug/Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.d
@@ -1,5 +1,5 @@
 Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.o: \
- C:/Users/uller/OneDrive\ -\ Danmarks\ Tekniske\ Universitet/Uni/Kandidat/Sommer\ semester/SpecialCourse/GIT/Pedictive-Maintenance/Projects/data_gather/Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.c \
+ C:/Users/bjs/Documents/DTU/Martin/Pedictive-Maintenance/Projects/data_gather/Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.c \
  ../../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h7xx.h \
  ../../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h755xx.h \
  ../../Drivers/CMSIS/Include/core_cm4.h \
